c335005089ba4601c9ba3f7cc9513a1305b64a79
sttinh99 <17110381@student.hcmute.edu.vn>
2020-08-20 21:48:57 +0700

    update register check mail

--- controller/register.controller.js
+++ controller/register.controller.js
@@ -8,10 +8,15 @@ module.exports.register = async function(req, res) {
     res.render('./register/register');
 }
 module.exports.postRegister = async function(req, res, next) {
-    var result = await cloudinary.v2.uploader.upload(req.file.path);
-    req.body.avatarUrl = result.secure_url;
-    req.body.wrongLoginCount = 0;
-    req.body.pass = "$2b$10$O2ULVrvXPg/j9onUzRDFWehcOihIcQ1ebpvxAjUyCdwnmTDdSqg9G"
-    await User.create(req.body);
-    res.redirect("/auth/login");
+    let x = await User.findOne({email:req.body.email})
+    if(!x){
+        var result = await cloudinary.v2.uploader.upload(req.file.path);
+        req.body.avatarUrl = result.secure_url;
+        req.body.wrongLoginCount = 0;
+        req.body.pass = "$2b$10$O2ULVrvXPg/j9onUzRDFWehcOihIcQ1ebpvxAjUyCdwnmTDdSqg9G"
+        await User.create(req.body);
+        res.redirect("/auth/login");
+        next();
+    }
+    res.render('./register/register',{errors:"email da ton tai"})
   }
